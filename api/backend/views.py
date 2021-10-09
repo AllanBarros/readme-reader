7d9bb2307fe74fb91ef3d0c9f1c90fa469be7f0e
AllanBarros <allanbmalves@gmail.com>
2021-10-09 17:42:41 -0300

    Levantamento de erros nas cahamadas das urls.

--- api/backend/views.py
+++ api/backend/views.py
@@ -2,26 +2,59 @@ from django.http.response import JsonResponse
 import requests
 import json
 
-def get_readme(request):
+def get_readme(request, slug):
+    """Função para busca e leitura dos readme's de uma organização"""
+    
     dados_readme = []
+    # 'allan-testing-organization'
 
-    lista_repositorios = get_repos_list()
-    for i, repositorio in enumerate(lista_repositorios):
-        url_repositorio = 'https://api.github.com/repos/allan-testing-organization/' + repositorio + '/readme'
-        result = requests.get(url_repositorio)
-        link_arquivo_html = json.loads(result.content)
-        arquivo = requests.get(link_arquivo_html['download_url'])
+    lista_repositorios = get_repos_list(slug)
+
+    for repositorio in lista_repositorios:
+
+        link_arquivo_html = get_readme_html_link(repositorio, slug)
+
+        arquivo = get_readme_content(link_arquivo_html, repositorio)    
+
         dados_readme.append(str(arquivo.content))
 
     return JsonResponse({'resultado':dados_readme})
 
-def get_repos_list():
-
+def get_repos_list(org):
+    """Função para busca de todos os repositórios dado uma organização específica"""
+    
     lista_repositorios = []
+    link_org = 'https://api.github.com/orgs/' + org +'/repos'
 
-    result = requests.get('https://api.github.com/orgs/allan-testing-organization/repos')
+    result = requests.get(link_org)
+
+    if result.status_code == 404:
+        result.raise_for_status()
+
     resultados = json.loads(result.content)
+    
     for repositorio in resultados:
         lista_repositorios.append(repositorio['name'])
+    
     return lista_repositorios
     
+def get_readme_html_link(repositorio, org):
+
+    url_repositorio = 'https://api.github.com/repos/'+ org + '/' + repositorio + '/readme'
+
+    result = requests.get(url_repositorio)
+    
+    if result.status_code == 404:
+        result.raise_for_status()
+        
+    link_arquivo_html = json.loads(result.content)
+    return link_arquivo_html
+
+def get_readme_content(link_arquivo_html):
+
+    arquivo = requests.get(link_arquivo_html['download_url'])
+    
+    if arquivo.status_code == 404:
+        arquivo.raise_for_status()
+
+    return arquivo
